b98c10526da0a0dfb2730985a1c40a4f2de11384
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-04 20:57:02 +0900

    add new file

---
+++ docRoot/user.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <a>col-md-6-1</a>
+      </div>
+      <div class="col-md-6">
+        <a>col-md-6-2</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
