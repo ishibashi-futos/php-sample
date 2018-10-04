6df9a5b52180a4c0b5f09c984d1083dd1dc67385
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-04 20:36:50 +0900

    add error page

---
+++ docRoot/error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Error Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div>
+      <div class="page-header text-center">
+          <h1>エラーが発生しました</br>
+              ログインページに戻ってください。
+          </h1>
+      </div>
+      <a href="/index.php">戻る</a>
+    </div>
+  </div>
+</body>
+</html>
