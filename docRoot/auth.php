af52557c918dc940e1a3037c8c00bd161757a7f8
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-04 20:18:23 +0900

    add new file

---
+++ docRoot/auth.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+<?php
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $authFile = fopen('userlist.csv', 'r');
+  $auth = false;
+  if($authFile) {
+    while($line = fgets($authFile)) {
+      $line = rtrim($line, "\n");    
+      $lines = explode(',', $line);
+      $userName = $lines[0];
+      $userPassword = $lines[1];
+      $role = $lines[2];
+      if($userName == $email && $userPassword == $password && $role == "admin") {
+        $auth = true;
+	//print '<meta http-equiv="refresh" content="0;URL=\'https://www.google.co.jp/\'" />';
+	print 'Welcome administrator!';
+	print base64_encode($userName . ',' . $userPassword . ',' . "admin");
+	break;
+      } else if ($userName == $email && $userPassword == $password && $role == "user") {
+        $auth = true;
+	//print '<meta http-equiv="refresh" content="0;URL=\'https://www.yahoo.co.jp/\'" />';
+	print 'Welcome user!';
+	break;
+      }
+    }
+  }
+?>
+</head>
+<body></body>
+</html>
---
+++ docRoot/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style>
+    @media screen and (min-width: 1280px) {
+        .card {
+            max-width: 350px;
+            padding: 40px 40px;
+        }
+        .container {
+            max-width: 430px;
+            margin-right: auto;
+            margin-left : auto;
+        }
+    }
+    .page-header {
+        border-bottom: double 1px #f0f0f0;
+        margin-bottom: 10px;
+    }
+    .form-control {
+        margin-bottom: 10px;
+    }
+    </style>
+</head>
+<body>
+    <div class="page-header text-center">
+        <h1>Login</h1>
+    </div>
+    <div class="container">
+        <div class="card card-container">
+            <form  action="/auth.php" method="post" class="form-signin mb10">
+                <input type="email" name="email" class="form-control mb10" placeholder="Email Address" requied autofocus />
+                <input type="password" name="password" class="form-control" placeholder="Password" required />
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
