bd4b88a67bf39da4fdffcd3323756bf9bc2ed41a
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-06 10:47:12 +0900

    change

--- docRoot/api/done.php
+++ docRoot/api/done.php
@@ -1,7 +1,9 @@
 <?php
 
-$_AUTH = isset($_POST["authKey"]) ? $_POST["authKey"] : null;
-$_WEEK = isset($_POST["week"]) ? $_POST["week"] : null;
-var_dump($_SERVER);
+$week = isset($_POST["week"]) ? $_POST["week"] : null;
+$authKey = $_SERVER["HTTP_AUTHKEY"];
+
+var_dump($authKey);
+var_dump($week);
 
 ?>
