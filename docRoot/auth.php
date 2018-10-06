eb9a93091a0257bde42edf29751cc678482514c6
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-06 11:30:42 +0900

    認証情報をCookieに保存

--- docRoot/auth.php
+++ docRoot/auth.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
 <?php
+  // 認証チェック
   $email = $_POST["email"];
   $password = $_POST["password"];
   $authFile = fopen('./data/userlist.csv', 'r');
@@ -13,17 +14,18 @@
       $userName = $lines[0];
       $userPassword = $lines[1];
       $role = $lines[2];
+      // 認証ユーザ内容チェック
       if($userName == $email && $userPassword == $password && $role == "admin") {
         $auth = true;
         //print '<meta http-equiv="refresh" content="0;URL=\'https://www.google.co.jp/\'" />';
-        print 'Welcome administrator!';
-	      print base64_encode($userName . ',' . $userPassword . ',' . "admin");
+        // cookieをセット、有効期限は1時間
+        setcookie('authKey', base64_encode($userName . ',' . $userPassword . ',' . "admin"), time() + 3600);
         break;
       } else if ($userName == $email && $userPassword == $password && $role == "user") {
         $auth = true;
         //print '<meta http-equiv="refresh" content="0;URL=\'https://www.yahoo.co.jp/\'" />';
-	      print base64_encode($userName . ',' . $userPassword . ',' . "user");
-        print 'Welcome user!';
+        // cookieをセット、有効期限は1時間
+        setcookie('authKey', base64_encode($userName . ',' . $userPassword . ',' . "user"), time() + 3600);
         break;
       }
     }
