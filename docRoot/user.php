8c1968c678c6fd2c8c08e88adef1a6327dce1d42
root <root@ip-172-31-20-138.ap-northeast-1.compute.internal>
2018-10-06 01:33:08 +0000

    change

--- docRoot/user.php
+++ docRoot/user.php
@@ -2,8 +2,11 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>Login Page</title>
+    <title>完了報告</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style>
+      
+    </style>
 </head>
 <body>
   <div class="container">
@@ -27,6 +30,7 @@
                 if($refFile) {
                   $refDate = new DateTime(rtrim(fgets($refFile), "\r"));
                 }
+                // 曜日を定義する
                 $arrays = array(
                   0 => "月",
                   1 => "火",
@@ -36,11 +40,27 @@
                   5 => "土",
                   6 => "日"
                 );
+                // 作業が必要な日、完了(1)/未完了(0)をファイルから取得する
+                $workScheduleFile = fopen('workSchedule.csv', 'r');
+                $schedule = array();
+                if($workScheduleFile) {
+                  while($line = fgets($workScheduleFile)) {
+                    $lines = explode(",", rtrim($line, "\r\n"));
+                    $schedule += array($lines[0]=>$lines[1]); 
+                  }
+                }
                 foreach($arrays as $key => $value) {
                   print "<tr>";
                   print "<td>{$refDate->format('Y年m月d日')}</td>";
                   print "<td>{$value}</td>";
-                  print "<td><button class='btn btn-lg btn-primary btn-block' type='submit'>完了</button></td>";
+                  // master情報に応じて、ボタンの表示、非表示を制御
+                  if($schedule[$key] == "0"){
+                    print "<td><button class='btn btn-sm btn-primary btn-block' type='submit'>完了</button></td>";
+                  } else if($schedule[$key] == "1") {
+                    print "<td><span class='label label-success'>完了</span></td>";
+                  } else {
+                    print "<td><span class='label label-default'>-</span></td>";
+                  }
                   print "</tr>";
                   $refDate->modify('+1 days');
                 }
