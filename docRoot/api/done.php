8f5767f8dea8b1f19e3e84085294e02253b005d0
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-06 12:38:42 +0900

    test commit

--- docRoot/api/done.php
+++ docRoot/api/done.php
@@ -20,7 +20,26 @@ if($authKey) {
 
 if($authorized) {
   $week = isset($_POST["week"]) ? $_POST["week"] : null;
-  var_dump($week);
+  $woskSchedulePath = "../data/workSchedult.csv";
+  $workScheduleFile = fopen($woskSchedulePath, 'r');
+  $resultArray = array();
+  if($workScheduleFile) {
+    while($line = fgets($workScheduleFile)) {
+      $line = rtrim($line, "\n");
+      $lines = explode(',', $line);
+      // 読込が完了したデータのうち、weekにマッチする行のステータスを1(終了)に書き換える.
+      if($lines[0] == $week) {
+        $resultArray += array($lines[0]=>1);
+      } else {
+        $resultArray += array($lines[0]=>$lines[1]);
+      }
+    }
+  }
+  fclose($workScheduleFile);
+
+  foreach($resultArray as $key => $value) {
+    file_put_contents($woskSchedulePath, "{$key},{$value}\n",LOCK_EX);
+  }
 }
 
 ?>
