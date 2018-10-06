9ddf3b9031a1bb0e9fed8500fd105d19b2254be1
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-06 10:58:08 +0900

    change

--- docRoot/api/done.php
+++ docRoot/api/done.php
@@ -1,9 +1,26 @@
 <?php
 
-$week = isset($_POST["week"]) ? $_POST["week"] : null;
-$authKey = $_SERVER["HTTP_AUTHKEY"];
+// ヘッダから認証キーをチェック.
+$authKey = isset($_SERVER["HTTP_AUTHKEY"]) ? $_SERVER["HTTP_AUTHKEY"] : null;
+$authorized = false;
 
-var_dump($authKey);
-var_dump($week);
+
+if($authKey) {
+  $authKeys = base64_decode($authKey);
+  $authKeys = explode(',', $authKeys);
+  // authKeyがuserでもadminでもない場合
+  if(!$authKeys[2] == "user" || !$authKeys[2] == "admin") {
+    http_response_code(401);
+  } else {
+    $authorized = true;
+  }
+} else {
+  http_response_code(401);
+}
+
+if($authorized) {
+  $week = isset($_POST["week"]) ? $_POST["week"] : null;
+  var_dump($week);
+}
 
 ?>
