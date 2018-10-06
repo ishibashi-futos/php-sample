b18d36ad02473925d7838c5523efdc3814b401a6
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-06 12:56:58 +0900

    try catch

--- docRoot/api/done.php
+++ docRoot/api/done.php
@@ -19,35 +19,40 @@ if($authKey) {
 }
 
 if($authorized) {
-  $week = isset($_POST["week"]) ? $_POST["week"] : null;
-  $woskSchedulePath = "../data/workSchedule.csv";
-  $workScheduleFile = fopen($woskSchedulePath, 'r');
-  $resultArray = array();
-  if($workScheduleFile) {
-    while($line = fgets($workScheduleFile)) {
-      $line = rtrim($line, "\n");
-      $lines = explode(',', $line);
-      // 読込が完了したデータのうち、weekにマッチする行のステータスを1(終了)に書き換える.
-      if($lines[0] == $week) {
-        $resultArray += array($lines[0]=>"1");
-      } else {
-        $resultArray += array($lines[0]=>$lines[1]);
+  try {
+    $week = isset($_POST["week"]) ? $_POST["week"] : null;
+    $woskSchedulePath = "../data/workSchedule.csv";
+    $workScheduleFile = fopen($woskSchedulePath, 'r');
+    $resultArray = array();
+    if($workScheduleFile) {
+      while($line = fgets($workScheduleFile)) {
+        $line = rtrim($line, "\n");
+        $lines = explode(',', $line);
+        // 読込が完了したデータのうち、weekにマッチする行のステータスを1(終了)に書き換える.
+        if($lines[0] == $week) {
+          $resultArray += array($lines[0]=>1);
+        } else {
+          $resultArray += array($lines[0]=>$lines[1]);
+        }
       }
     }
-  }
-  fclose($workScheduleFile);
-  var_dump($resultArray);
-
-  $isFirst = true;
-  foreach($resultArray as $key => $value) {
-    if($isFirst) {
-      // 最初は頭から書き込み
-      file_put_contents($woskSchedulePath, "{$key},{$value}\n",LOCK_EX);
-      $isFirst = false;
-    } else {
-      // つぎから追記
-      file_put_contents($woskSchedulePath, "{$key},{$value}\n",FILE_APPEND | LOCK_EX);
+    // fileStreamをclose
+    fclose($workScheduleFile);
+  
+    $isFirst = true;
+    foreach($resultArray as $key => $value) {
+      if($isFirst) {
+        // 最初は頭から書き込み
+        file_put_contents($woskSchedulePath, "{$key},{$value}\n",LOCK_EX);
+        $isFirst = false;
+      } else {
+        // つぎから追記
+        file_put_contents($woskSchedulePath, "{$key},{$value}\n",FILE_APPEND | LOCK_EX);
+      }
     }
+    http_response_code(201);
+  } catch(Exception $e) {
+    http_response_code(500);
   }
 }
 
