0f8a3143321dad80ea7b80e189d36055e25179d7
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-07 10:52:10 +0900

    完了ラベルを追加、ボタンを未完了に変更

--- docRoot/user.php
+++ docRoot/user.php
@@ -57,9 +57,9 @@
                   print "<td>{$value}</td>";
                   // master情報に応じて、ボタンの表示、非表示を制御
                   if($schedule[$key] == "0"){
-                    print "<td><button class='btn btn-sm btn-primary btn-block' type='button' onClick='doneWork({$key});'>完了</button></td>";
+                    print "<td><button class='btn btn-sm btn-primary btn-block' type='button' onClick='doneWork({$key});'>未完了</button></td>";
                   } else if($schedule[$key] == "1") {
-                    print "<td><span class='label label-success'>完了</span></td>";
+                    print "<td><label class='label label-success'>完了</label></td>";
                   } else {
                     print "<td><span class='label label-default'>-</span></td>";
                   }
