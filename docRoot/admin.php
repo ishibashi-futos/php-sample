5d9d02300623a828d2ac087f55f07f5d81d6fbf4
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-07 13:53:46 +0900

    add src

--- docRoot/admin.php
+++ docRoot/admin.php
@@ -10,6 +10,8 @@
     <!-- DatePicker -->
     <script src="bootstrap-datepicker/bootstrap-datepicker.ja.min.js"></script>
     <script src="bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="js/admin.js"></script>
+    <script src="js/common.js"></script>
     <link rel="stylesheet" href="bootstrap-datepicker/bootstrap-datepicker.min.css">
     <style>
     </style>
