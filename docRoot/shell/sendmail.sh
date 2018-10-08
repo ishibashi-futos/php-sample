09c4996b3040f66f5a4a037f356862057d676859
ishibashi.futos <ishibashi.futos@outlook.com>
2018-10-08 12:07:21 +0900

    commit

--- docRoot/shell/sendmail.sh
+++ docRoot/shell/sendmail.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 # メールの送信チェック、送信処理を行う
 
+# カレントに移動
+cd $(dirname $0)
+source ../data/alertControl.ini
+refDate=$(echo "${refDate}" 23:59:59)
+if [ $(date -d "${refDate}" "+%s") -gt $(date "+%s") ]; then
+  if [ "${alertControl}" -eq "1" ]; then
+    echo 送信不要
+    exit 0
+  fi
+fi
+
+# スケジュールを読み取る
+for line in $(cat ../data/workSchedule.csv) ; do
+  col1=$(echo $line | cut -d ',' -f 1 | xargs -I %a expr %a + 1)
+  col2=$(echo $line | cut -d ',' -f 2)
+  # 曜日が一致し、かつcol2が0以外の場合は送信不要としてexit
+  if [ "${col1}" -eq $(date "+%u") ] ; then
+    if [ "${col2}" != "0" ] ; then
+      echo 送信不要
+      exit 0
+    fi
+  fi
+done
+
+# メールを送る
+source ./mail.dat
+
+{
+  echo "From: ${FROM_ADDRESS}"
+  echo "To: ${TO_ADDRESS}"
+  echo "Subject: ${TITLE_TEXT}"
+  echo 
+  echo "${BODY_TEXT}"
+} | sendmail -i -t
--- docRoot/shell/weeklytasks.sh
+++ docRoot/shell/weeklytasks.sh
@@ -6,8 +6,10 @@ cd $(dirname $0)
 
 WEEK=$(date "+%u")
 
+# 一応月曜日か確認する
 if [ "${WEEK}" -ne "1" ] ; then
   exit 0;
 fi
 
+# 基準日を書き換える
 date "+%Y-%m-%d" > ../conf/refDate.ini
